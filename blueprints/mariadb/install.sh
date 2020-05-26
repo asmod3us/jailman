bb1bbf458d95056dd5453bfd4e4f2d3450f30b94
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-05-27 00:34:59 +0200

    Exit function and some small fixes - remove some verbosity - fix: Its poller, not unifi_poller - Document functions - tandardise includes copying Fixes #109 - Add standardised exit function - Output custom or standardised success message - Include connection instructions or wiki reference - Create a "INSTALLED" file for future reinstall reference - Remove custom success messages from blueprints

--- blueprints/mariadb/install.sh
+++ blueprints/mariadb/install.sh
@@ -25,10 +25,6 @@ zfs set primarycache=metadata "${global_dataset_config}"/"${1}"/db
 
 iocage exec "${1}" chown -R 88:88 /var/db/mysql
 
-# Install includes fstab
-iocage exec "${1}" mkdir -p /mnt/includes
-iocage fstab -a "${1}" "${includes_dir}" /mnt/includes nullfs rw 0 0
-
 iocage exec "${1}" mkdir -p /usr/local/www/phpmyadmin
 iocage exec "${1}" chown -R www:www /usr/local/www/phpmyadmin
 
@@ -49,8 +45,8 @@ iocage exec "${1}" sysrc mysql_enable="YES"
 
 # Copy and edit pre-written config files
 echo "Copying Caddyfile for no SSL"
-iocage exec "${1}" cp -f /mnt/includes/caddy.rc /usr/local/etc/rc.d/caddy
-iocage exec "${1}" cp -f /mnt/includes/Caddyfile /usr/local/www/Caddyfile
+cp "${includes_dir}"/caddy.rc /mnt/"${global_dataset_iocage}"/jails/"$1"/root/usr/local/etc/rc.d/caddy
+cp "${includes_dir}"/Caddyfile /mnt/"${global_dataset_iocage}"/jails/"$1"/root/usr/local/www/Caddyfile
 iocage exec "${1}" sed -i '' "s/yourhostnamehere/${host_name}/" /usr/local/www/Caddyfile
 iocage exec "${1}" sed -i '' "s/JAIL-IP/${ip4_addr%/*}/" /usr/local/www/Caddyfile
 
@@ -67,7 +63,7 @@ if [ "${REINSTALL}" == "true" ]; then
 else
 	
 	# Secure database, set root password, create Nextcloud DB, user, and password
-	iocage exec "${1}" cp -f /mnt/includes/my-system.cnf /var/db/mysql/my.cnf
+	cp "${includes_dir}"/my-system.cnf /mnt/"${global_dataset_iocage}"/jails/"$1"/root/var/db/mysql/my.cnf
 	iocage exec "${1}" mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
 	iocage exec "${1}" mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 	iocage exec "${1}" mysql -u root -e "DROP DATABASE IF EXISTS test;"
@@ -75,20 +71,14 @@ else
 	iocage exec "${1}" mysqladmin --user=root password "${root_password}"
 	iocage exec "${1}" mysqladmin reload
 	fi
-iocage exec "${1}" cp -f /mnt/includes/my.cnf /root/.my.cnf
+cp "${includes_dir}"/my.cnf /mnt/"${global_dataset_iocage}"/jails/"$1"/root/root/.my.cnf
 iocage exec "${1}" sed -i '' "s|mypassword|${root_password}|" /root/.my.cnf
 
 # Save passwords for later reference
 iocage exec "${1}" echo "MariaDB root password is ${root_password}" > /root/"${1}"_root_password.txt
-	
 
-# Don't need /mnt/includes any more, so unmount it
-iocage fstab -r "${1}" "${includes_dir}" /mnt/includes nullfs rw 0 0
-
-# Done!
-echo "Installation complete!"
-echo "Using your web browser, go to http://${host_name} to log in"
-
+exitblueprint "$1" "MariaDB is now accessible at http://${ip4_addr%/*}"
+echo "All passwords are saved in /root/${1}_db_password.txt"
 if [ "${REINSTALL}" == "true" ]; then
 	echo "You did a reinstall, please use your old database and account credentials"
 else
@@ -97,4 +87,3 @@ else
 	echo "The MariaDB root password is ${root_password}"
 	fi
 echo ""
-echo "All passwords are saved in /root/${1}_db_password.txt"
