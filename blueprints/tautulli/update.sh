adf28048cb9ba6f531d4a86ea1db89b394b7b244
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-05-27 11:33:20 +0200

    Blueprint Init refactor (#120)
    * Refactor Blueprint Initialisation and variable creation.
    
    - add mariadb required reqvars
    - update example config
    - Consolidate MariaDB
    - Consolidate Nextcloud
    - Remove old database options from MariaDB
    - Redo Unifi structure
    - Refactor Bitwarden blueprint
    - remove global ignored shellcheck errors
     - add global vars (always available to every blueprint)
    - Move included functions to /includes
    - Move dev-only global config from config.yml to ./includes/global.yml
    - move globals to includes dir
    - remove some vars that are now covered by the init script
    - add includes_dir variable to init script
    - Add required vars check before jail creation
    - AddUpdate auto branch autodetect
    - Add example jail
    
    * remove duplicate folder deletion command and broken service-stop
    
    * Document all the standarisations and options
    
    * init the blueprint, not the jail

--- blueprints/tautulli/update.sh
+++ blueprints/tautulli/update.sh
@@ -1,6 +1,11 @@
 #!/usr/local/bin/bash
 # This file contains the update script for Tautulli
 
+#init jail
+initblueprint "$1"
+
+# Initialise defaults
+
 iocage exec "$1" service tautulli stop
 # Tautulli is updated through pkg, this is mostly just a placeholder
 iocage exec "$1" chown -R tautulli:tautulli /usr/local/share/Tautulli /config
--- blueprints/unifi/update.sh
+++ blueprints/unifi/update.sh
@@ -2,15 +2,16 @@
 # This file contains the update script for unifi
 # Unifi Controller is updated through pkg, Unifi-Poller is not. This script updates Unifi-Poller
 
-if [[ ! "${!POLLER}" ]]; then
+#init jail
+initblueprint "$1"
+
+if [[ ! "${unifi_poller}" ]]; then
 	echo "Skipping Unifi Poller for update, not installed"
 else
-	
 	FILE_NAME=$(curl -s https://api.github.com/repos/unifi-poller/unifi-poller/releases/latest | jq -r ".assets[] | select(.name | contains(\"amd64.txz\")) | .name")
 	DOWNLOAD=$(curl -s https://api.github.com/repos/unifi-poller/unifi-poller/releases/latest | jq -r ".assets[] | select(.name | contains(\"amd64.txz\")) | .browser_download_url")
 	
 	# Check to see if there is an update.
-	# shellcheck disable=SC2154
 	if [[ -f /mnt/"${global_dataset_config}"/"${1}"/"${FILE_NAME}" ]]; then
 		echo "Unifi-Poller is up to date."
 		exit 1
@@ -18,7 +19,6 @@ else
 		# Download and install the package
 		iocage exec "${1}" fetch -o /config "${DOWNLOAD}"
 		iocage exec "${1}" pkg install -qy /config/"${FILE_NAME}"
-		
 		iocage exec "${1}" service unifi_poller restart
 	fi
 fi
