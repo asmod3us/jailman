66741092b2ad8c33de6099acc5f8c468f574f0b2
asmod3us <4sm0d3us@gmail.com>
2020-05-30 17:28:05 +0200

    add install script

--- blueprints/sabnzbd3/install.sh
+++ blueprints/sabnzbd3/install.sh
@@ -1,17 +1,28 @@
 #!/usr/local/bin/bash
 # This file contains an example install script to base your own jails on
 
+set -o errexit   # Exit on most errors
+set -o errtrace  # Make sure any error trap is inherited
+set -o nounset   # Disallow expansion of unset variables
+set -o pipefail  # Use last non-zero exit code in a pipeline
+
+trap "ERR during sabnzbd3 install" ERR
+
 initblueprint "$1"
 
-# Initialise defaults
-# You can add default values for the variables already loaded here.
+source_version=3.0.0Beta1
+archive=${source_version}.tar.gz
+target=/usr/local/share/sabnzbd3
+iocage exec "$1" mkdir -p $target
+iocage exec "$1" curl -qsL https://github.com/sabnzbd/sabnzbd/archive/$archive | tar -xvzf - --strip-components 1 -C $target
+cp "${includes_dir}"/build.sh /mnt/"${global_dataset_iocage}"/jails/"$1"/root/root/
+iocage exec "$1" bash /root/build.sh $target
+iocage exec "$1" cp $target/SABnzbd.py /usr/local/bin/
+cp "${includes_dir}"/sabnzbd3.rc /usr/local/etc/rc.d/sabnzbd
+iocage exec "$1" chmod +x /usr/local/etc/rc.d/sabnzbd
 
-if [ "${reinstall}" = "true" ]; then
-	echo "Reinstall detected..."
-else
-	echo "no reinstall detected, normal install proceeding..."
-fi
+iocage exec "$1" pw user add sabnzbd3 -c sabnzbd3 -d /nonexistent -s /usr/bin/nologin
+iocage exec "$1" sysrc "sabnzbd_enable=YES"
+iocage exec "$1" service sabnzbd start
 
-iocage exec "$1" curl -LO https://github.com/sabnzbd/sabnzbd/archive/3.0.0Beta1.tar.gz
-
-exitblueprint "$1" "SABnzbd3 is now available at http://:8080/"
+exitblueprint "$1" "SABnzbd3 is now available at http://${jail_ip}:/"
