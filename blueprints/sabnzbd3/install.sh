13911e9358fe5cfc13b8e104d5f3912274e7afc3
asmod3us <4sm0d3us@gmail.com>
2020-05-30 22:29:08 +0200

    enable service early

--- blueprints/sabnzbd3/install.sh
+++ blueprints/sabnzbd3/install.sh
@@ -22,6 +22,7 @@ cp "${includes_dir}"/sabnzbd3.rc "${jail_root}"/usr/local/etc/rc.d/sabnzbd
 iocage exec "$1" chmod +x /usr/local/etc/rc.d/sabnzbd
 iocage exec "$1" pw user add sabnzbd -c sabnzbd -d /nonexistent -s /usr/bin/nologin
 iocage exec "$1" chown -R sabnzbd:sabnzbd  /config
+iocage exec "$1" sysrc "sabnzbd_enable=YES"
 
 # start once to let service write default config
 iocage exec "$1" service sabnzbd start
@@ -32,7 +33,6 @@ iocage exec "$1" sed -i '' -e 's?host = 127.0.0.1?host = 0.0.0.0?g' /config/sabn
 iocage exec "$1" sed -i '' -e 's?download_dir = Downloads/incomplete?download_dir = /mnt/downloads/incomplete?g' /config/sabnzbd.ini
 iocage exec "$1" sed -i '' -e 's?complete_dir = Downloads/complete?complete_dir = /mnt/downloads/complete?g' /config/sabnzbd.ini
 
-iocage exec "$1" sysrc "sabnzbd_enable=YES"
 iocage exec "$1" service sabnzbd start
 
 exitblueprint "$1" "SABnzbd3 is now available at http://${jail_ip}:8080/"
