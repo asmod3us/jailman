b39cf545089606e3c8af2edbcca267a6c873fabf
asmod3us <4sm0d3us@gmail.com>
2020-05-30 22:22:18 +0200

    put config in place

--- blueprints/sabnzbd3/install.sh
+++ blueprints/sabnzbd3/install.sh
@@ -18,11 +18,20 @@ iocage exec "$1" mkdir -p $target
 iocage exec "$1" "curl -qsL https://github.com/sabnzbd/sabnzbd/archive/"$archive" | tar -xvzf - --strip-components 1 -C $target"
 cp "${includes_dir}"/build.sh "${jail_root}"/root/
 iocage exec "$1" bash /root/build.sh $target
-
 cp "${includes_dir}"/sabnzbd3.rc "${jail_root}"/usr/local/etc/rc.d/sabnzbd
 iocage exec "$1" chmod +x /usr/local/etc/rc.d/sabnzbd
-
 iocage exec "$1" pw user add sabnzbd -c sabnzbd -d /nonexistent -s /usr/bin/nologin
+iocage exec "$1" chown -R sabnzbd:sabnzbd  /config
+
+# start once to let service write default config
+iocage exec "$1" service sabnzbd start
+iocage exec "$1" service sabnzbd stop
+
+# put our config in place
+iocage exec "$1" sed -i '' -e 's?host = 127.0.0.1?host = 0.0.0.0?g' /config/sabnzbd.ini
+iocage exec "$1" sed -i '' -e 's?download_dir = Downloads/incomplete?download_dir = /mnt/downloads/incomplete?g' /config/sabnzbd.ini
+iocage exec "$1" sed -i '' -e 's?complete_dir = Downloads/complete?complete_dir = /mnt/downloads/complete?g' /config/sabnzbd.ini
+
 iocage exec "$1" sysrc "sabnzbd_enable=YES"
 iocage exec "$1" service sabnzbd start
 
