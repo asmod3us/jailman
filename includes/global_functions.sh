cfb8894a4cdd810ebaa72efa871148b7686858cd
asmod3us <4sm0d3us@gmail.com>
2020-06-01 15:48:05 +0200

    also check for leading whitespace

--- includes/global_functions.sh
+++ includes/global_functions.sh
@@ -28,7 +28,7 @@ parse_yaml() {
 
 # $1: config string to validate
 validate_config() {
-	{ sed -e s'/export //' | awk -F= '$2 ~ /.*[[:space:]]"$/ { print "Warning: Key " $1 " has trailing whitespace: " $2 "" }'; } <<< "$1"
+	{ sed -e s'/export //' | awk -F= '$2 ~ /.*[[:space:]]"$/ || $2 ~ /^"[[:space:]].*/ { print "Warning: Key " $1 " has trailing whitespace: " $2 "" }'; } <<< "$1"
 }
 
 # automatic update function
